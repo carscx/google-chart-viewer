c7b47efe4edde529aeccb5e575915873e98dc289
Carlos Santos <karscx@gmail.com>
2024-07-03 16:13:14 -0300

    Fix version

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,20 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'GoogleChartComponent',
+      fileName: (format) => `google-chart-component.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
 })
